110aa28998d2dfe09435baed1db29b30242848a1
thpun <th.pun.1994@gmail.com>
2019-05-09 00:49:05 +0800

    Add serialization & de-serialization of PublicKey, DecryptKey, MasterKey, Ciphertext; with test

--- gpsw06/types.go
+++ gpsw06/types.go
@@ -1,6 +1,9 @@
 package gpsw06
 
 import (
+	"encoding/base64"
+	"encoding/json"
+
 	"github.com/Nik-U/pbc"
 )
 
@@ -16,16 +19,34 @@ type PublicKey struct {
 	y *GT
 }
 
+type publicKey struct {
+	KeyType string   `json:"type"`
+	T       [][]byte `json:"t"`
+	Y       []byte   `json:"y"`
+}
+
 type DecryptKey struct {
 	// contains filtered or unexported fields
 	d    map[int]*G1
 	tree []byte
 }
 
+type decryptKey struct {
+	KeyType string         `json:"type"`
+	D       map[int][]byte `json:"d"`
+	Tree    []byte         `json:"tree"`
+}
+
 type MasterKey struct {
 	// contains filtered or unexported fields
 	t []*Zr
 	y *Zr
+}
+
+type masterKey struct {
+	KeyType string   `json:"type"`
+	T       [][]byte `json:"t"`
+	Y       []byte   `json:"y"`
 }
 
 type Message struct {
@@ -40,12 +61,152 @@ type Ciphertext struct {
 	encAttrs map[int]*G2
 }
 
+type ciphertext struct {
+	Msg   []byte         `json:"msg"`
+	Attrs map[int][]byte `json:"attrs"`
+}
+
 type GPSW06 struct {
 	universe []Attribute
 }
 
 type polynomial struct {
 	c []*Zr
+}
+
+// Marshal converts pk into a byte slice.
+func (pk *PublicKey) Marshal() ([]byte, error) {
+	t := make([][]byte, 0)
+	for i := range pk.t {
+		t = append(t, pk.t[i].Bytes())
+	}
+
+	var y = pk.y.Bytes()
+
+	str, err := json.Marshal(publicKey{"public", t, y})
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(base64.StdEncoding.EncodeToString(str)), nil
+}
+
+// Unmarshal set pk to the result of converting the output of Marshal back into
+// a public key structure and then return b.
+func (pk *PublicKey) Unmarshal(b []byte) ([]byte, error) {
+	str, err := base64.StdEncoding.DecodeString(string(b))
+	if err != nil {
+		return nil, err
+	}
+
+	var instance = publicKey{}
+	if err := json.Unmarshal([]byte(str), &instance); err != nil {
+		return nil, err
+	} else if instance.KeyType != "public" {
+		return nil, ErrExpectingPublicKey
+	}
+
+	var y *GT
+
+	t := make([]*G2, 0)
+
+	for i := range instance.T {
+		t = append(t, pairing.NewG2().SetBytes(instance.T[i]))
+	}
+	y = pairing.NewGT().SetBytes(instance.Y)
+
+	pk.t = t
+	pk.y = y
+
+	return b, nil
+}
+
+// Marshal converts dk into a byte slice.
+func (dk *DecryptKey) Marshal() ([]byte, error) {
+	d := make(map[int][]byte)
+	for k, v := range dk.d {
+		d[k] = v.Bytes()
+	}
+
+	str, err := json.Marshal(decryptKey{"private", d, dk.tree})
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(base64.StdEncoding.EncodeToString(str)), nil
+}
+
+// Unmarshal set dk to the result of converting the output of Marshal back into
+// a private key structure and then return b.
+func (dk *DecryptKey) Unmarshal(b []byte) ([]byte, error) {
+	str, err := base64.StdEncoding.DecodeString(string(b))
+	if err != nil {
+		return nil, err
+	}
+
+	var instance = decryptKey{}
+	if err := json.Unmarshal([]byte(str), &instance); err != nil {
+		return nil, err
+	} else if instance.KeyType != "private" {
+		return nil, ErrExpectingPrivateKey
+	}
+
+	d := make(map[int]*G1)
+
+	for k, v := range instance.D {
+		d[k] = pairing.NewG1().SetBytes(v)
+	}
+
+	dk.d = d
+	dk.tree = instance.Tree
+
+	return b, nil
+}
+
+// Marshal converts msk into a byte slice.
+func (msk *MasterKey) Marshal() ([]byte, error) {
+	t := make([][]byte, 0)
+	for i := range msk.t {
+		t = append(t, msk.t[i].Bytes())
+	}
+
+	var y = msk.y.Bytes()
+
+	str, err := json.Marshal(publicKey{"master", t, y})
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(base64.StdEncoding.EncodeToString(str)), nil
+}
+
+// Unmarshal set msk to the result of converting the output of Marshal back into
+// a master key structure and then return b.
+func (msk *MasterKey) Unmarshal(b []byte) ([]byte, error) {
+	str, err := base64.StdEncoding.DecodeString(string(b))
+	if err != nil {
+		return nil, err
+	}
+
+	var instance = masterKey{}
+	if err := json.Unmarshal([]byte(str), &instance); err != nil {
+		return nil, err
+	} else if instance.KeyType != "master" {
+		return nil, ErrExpectingMasterKey
+	}
+
+	var y *Zr
+	t := make([]*Zr, 0)
+
+	for i := range instance.T {
+		t = append(t, pairing.NewZr().SetBytes(instance.T[i]))
+	}
+	y = pairing.NewZr().SetBytes(instance.Y)
+
+	msk.t = t
+	msk.y = y
+
+	return b, nil
 }
 
 // NewMessage creates an empty Message.
@@ -70,6 +231,51 @@ func (msg *Message) Marshal() []byte {
 // a group element and then returns msg.
 func (msg *Message) Unmarshal(b []byte) ([]byte, error) {
 	return msg.m.SetBytes(b).Bytes(), nil
+}
+
+// Marshal converts ct into a byte slice.
+func (ct *Ciphertext) Marshal() ([]byte, error) {
+	m := ct.encMsg.Bytes()
+	a := make(map[int][]byte)
+	for k, v := range ct.encAttrs {
+		a[k] = v.Bytes()
+	}
+
+	str, err := json.Marshal(ciphertext{m, a})
+	if err != nil {
+		return nil, err
+	}
+
+	return []byte(base64.StdEncoding.EncodeToString(str)), nil
+}
+
+// Unmarshal set ct to the result of converting the output of Marshal back into
+// a ciphertext structure and then return b.
+func (ct *Ciphertext) Unmarshal(b []byte) ([]byte, error) {
+	str, err := base64.StdEncoding.DecodeString(string(b))
+	if err != nil {
+		return nil, err
+	}
+
+	var instance = ciphertext{}
+	if err := json.Unmarshal([]byte(str), &instance); err != nil {
+		return nil, err
+	}
+
+	m := pairing.NewGT().SetBytes(instance.Msg)
+	a := make(map[int]struct{})
+	encAttrs := make(map[int]*G2)
+
+	for k, v := range instance.Attrs {
+		a[k] = struct{}{}
+		encAttrs[k] = pairing.NewG2().SetBytes(v)
+	}
+
+	ct.attrs = a
+	ct.encMsg = m
+	ct.encAttrs = encAttrs
+
+	return b, nil
 }
 
 func newPolynomial(deg int) *polynomial {
--- gpsw06/types_test.go
+++ gpsw06/types_test.go
@@ -1,6 +1,9 @@
 package gpsw06
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestEvaluate1(t *testing.T) {
 	var (
@@ -49,5 +52,187 @@ func TestEvaluate3(t *testing.T) {
 
 	if !(f.evaluate(zero).Equals(num) && f.evaluate(one).Equals(num)) {
 		t.Errorf("Polynomial (degree 1, big number) evaluated wrongly.")
+	}
+}
+
+func TestPublicKey_Marshal(t *testing.T) {
+	pk := PublicKey{[]*G2{
+		pairing.NewG2().Rand(),
+		pairing.NewG2().Rand(),
+		pairing.NewG2().Rand(),
+		pairing.NewG2().Rand(),
+		pairing.NewG2().Rand(),
+		pairing.NewG2().Rand(),
+		pairing.NewG2().Rand(),
+	}, pairing.NewGT().Rand()}
+
+	pkStr, err := pk.Marshal()
+	if err != nil {
+		t.Errorf("Error occurred during marshalling public key: %v", err)
+	}
+
+	pk2 := PublicKey{}
+	if _, err := pk2.Unmarshal(pkStr); err != nil {
+		t.Errorf("Error occurred during unmarshalling public key: %v", err)
+	}
+
+	if len(pk.t) != len(pk2.t) {
+		t.Errorf("Public key length not match")
+	}
+
+	eq := true
+
+	for i := range pk.t {
+		eq = eq && pk.t[i].Equals(pk2.t[i])
+	}
+
+	if !eq {
+		t.Errorf("Public key t value not match")
+	}
+
+	if !pk.y.Equals(pk2.y) {
+		t.Errorf("Publick key y value not match")
+	}
+}
+
+func TestMasterKey_Marshal(t *testing.T) {
+	msk := MasterKey{[]*Zr{
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+		pairing.NewZr().Rand(),
+	}, pairing.NewZr().Rand()}
+
+	mskStr, err := msk.Marshal()
+	if err != nil {
+		t.Errorf("Error occurred during marshalling master key: %v", err)
+	}
+
+	msk2 := MasterKey{}
+	if _, err := msk2.Unmarshal(mskStr); err != nil {
+		t.Errorf("Error occurred during unmarshalling master key: %v", err)
+	}
+
+	if len(msk.t) != len(msk2.t) {
+		t.Errorf("Master key length not match")
+	}
+
+	eq := true
+
+	for i := range msk.t {
+		eq = eq && msk.t[i].Equals(msk2.t[i])
+	}
+
+	if !eq {
+		t.Errorf("Master key t value not match")
+	}
+
+	if !msk.y.Equals(msk2.y) {
+		t.Errorf("Master key y value not match")
+	}
+}
+
+func TestDecryptKey_Marshal(t *testing.T) {
+	d := make(map[int]*G1)
+	d[1] = pairing.NewG1().Rand()
+	d[3] = pairing.NewG1().Rand()
+	d[30] = pairing.NewG1().Rand()
+	d[302] = pairing.NewG1().Rand()
+	d[61303] = pairing.NewG1().Rand()
+	d[94613] = pairing.NewG1().Rand()
+	d[111113] = pairing.NewG1().Rand()
+	d[42584] = pairing.NewG1().Rand()
+	d[354] = pairing.NewG1().Rand()
+
+	dk := DecryptKey{d, []byte("tree")}
+
+	dkStr, err := dk.Marshal()
+	if err != nil {
+		t.Errorf("Error occurred during marshalling private key: %v", err)
+	}
+
+	dk2 := DecryptKey{}
+	if _, err := dk2.Unmarshal(dkStr); err != nil {
+		t.Errorf("Error occurred during unmarshalling private key: %v", err)
+	}
+
+	if bytes.Compare(dk.tree, dk2.tree) != 0 {
+		t.Errorf("Private key tree value not match.\n\t1: %v\n\t2: %v", dk.tree, dk2.tree)
+	}
+
+	if len(dk.d) != len(dk2.d) {
+		t.Errorf("Private key map size not match")
+	}
+
+	eq := true
+	for i, v := range dk.d {
+		v2, ok := dk2.d[i]
+		eq = eq && ok && v.Equals(v2)
+	}
+
+	if !eq {
+		t.Errorf("Private key d value not match")
+	}
+}
+
+func TestCiphertext_Marshal(t *testing.T) {
+	a := make(map[int]struct{})
+	ea := make(map[int]*G2)
+
+	for i := range []int{1, 2, 6, 9, 11, 99, 1654, 889416, 654981} {
+		a[i] = struct{}{}
+		ea[i] = pairing.NewG2().Rand()
+	}
+
+	ct := Ciphertext{a, pairing.NewGT().Rand(), ea}
+
+	ctStr, err := ct.Marshal()
+	if err != nil {
+		t.Errorf("Error occurred during marshalling ciphertext: %v", err)
+	}
+
+	ct2 := Ciphertext{}
+	if _, err := ct2.Unmarshal(ctStr); err != nil {
+		t.Errorf("Error occurred during unmarshalling ciphertext: %v", err)
+	}
+
+	if !ct.encMsg.Equals(ct2.encMsg) {
+		t.Errorf("Private key msg value not match")
+	}
+
+	if len(ct.attrs) != len(ct2.attrs) {
+		t.Errorf("Private key attrs size not match")
+	}
+
+	eq := true
+	for i := range ct.attrs {
+		_, ok := ct2.attrs[i]
+		eq = eq && ok
+	}
+
+	if !eq {
+		t.Errorf("Ciphertext attrs value not match")
+	}
+
+	if len(ct.encAttrs) != len(ct2.encAttrs) {
+		t.Errorf("Private key encAttrs size not match")
+	}
+
+	for i, v := range ct.encAttrs {
+		v2, ok := ct2.encAttrs[i]
+		eq = eq && ok && v.Equals(v2)
+	}
+
+	if !eq {
+		t.Errorf("Ciphertext encAttrs value not match")
 	}
 }
